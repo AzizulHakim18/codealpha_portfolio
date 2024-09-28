bea8ac9ca9948e0e585a954e4b02cf4b4cf9531a
AzizulHakim18 <hakimrony18@gamil.com>
2024-09-28 14:50:30 +0600

    about contact section added

---
+++ script/script.js
@@ -0,0 +1,8 @@
+// JavaScript for toggling the navigation menu
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
